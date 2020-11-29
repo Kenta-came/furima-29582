759c306f2c8b2f0ff7cf0a0f1b191f7e3210f4f3
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-11-29 21:50:12 +0900

    ビュー画面の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    added_attrs = [:nick_name,:email,:encrypted_password,:password, :last_name, :first_name,:last_name_kana, :first_name_kana, :birthday]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
