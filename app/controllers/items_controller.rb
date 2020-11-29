10bb93196001066ee0221250617190e4a97ead4b
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-11-29 23:19:56 +0900

    destroy,updateクラスの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,15 @@ class ItemsController < ApplicationController
     Item.create(item_params)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
   private
   def item_params
     params.require(:item).permit(:name, :image, :text)
