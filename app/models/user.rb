87d1d525b68be9231b92cb1e2f2a36dcb6258ef4
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-06 17:18:25 +0900

    バリデーションの記述追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  validates :nickname, presence: {message: "is invalid. Input nickname."}
+  validates :email, presence: true, uniqueness:{ case_sensitive: false }
+
+  with_options presence: true do
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input first_name."}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input last_name."}
+    
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input first_name_kana."}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input last_name_kana."}
+  end
+
+  validates :birthday, presence: {message: "is invalid. Input birthday."}
 end
