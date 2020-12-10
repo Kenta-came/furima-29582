bc3d6863b13d48f1f5ddd32660a70a5b666867c0
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-10 16:26:14 +0900

    ユーザー管理機能の単体テストコード

--- Gemfile
+++ Gemfile
@@ -62,5 +62,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pry-rails'
 
 gem 'devise'
-gem 'devise-i18n'
-gem 'devise-i18n-views'
+
--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Furima29582
   class Application < Rails::Application
-
-    config.i18n.default_locale = :ja  
+    
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
