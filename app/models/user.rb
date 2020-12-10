48150408737654f27ae6fe8da0e58e14b9910cce
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-10 17:37:20 +0900

    バリデーションをまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  validates :nickname, presence: {message: "is invalid. Input nickname."}
-  validates :encrypted_password,     presence: true, length: { minimum: 7 }
-  validates :email, presence: true, uniqueness:{ case_sensitive: false }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: {message: "is invalid. Input birthday."}
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email, uniqueness:{ case_sensitive: false }
+    validates :encrypted_password, length: { minimum: 7 }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
