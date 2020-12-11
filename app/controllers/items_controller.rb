ef86c00fa84b4a10cfb84ce612456676088bfa8e
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-11 16:20:27 +0900

    ユーザー管理機能に必要ないタスクを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,45 +1,45 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :destroy, :update]
-  before_action :move_to_index, except: [:index, :show, :search]
+#   before_action :set_item, only: [:edit, :show, :destroy, :update]
+#   before_action :move_to_index, except: [:index, :show, :search]
 
-  def index
-    @items = Item.all
-  end
+#   def index
+#     @items = Item.all
+#   end
 
-  def new
-    @item = Item.new
-  end
+#   def new
+#     @item = Item.new
+#   end
 
-  def create
-    Item.create(item_params)
-  end
+#   def create
+#     Item.create(item_params)
+#   end
 
-  def destroy
-    item.destroy
-  end
+#   def destroy
+#     item.destroy
+#   end
 
-  def update
-    item.update(item_params)
-  end
+#   def update
+#     item.update(item_params)
+#   end
 
-  def edit
-  end
+#   def edit
+#   end
 
-  def show
-  end
+#   def show
+#   end
 
-  private
-  def item_params
-    params.require(:item).permit(:name, :image, :text)
-  end
+#   private
+#   def item_params
+#     params.require(:item).permit(:name, :image, :text)
+#   end
 
-  def set_item
-    item = Item.find(params[:id])
-  end
+#   def set_item
+#     item = Item.find(params[:id])
+#   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-end
+#   def move_to_index
+#     unless user_signed_in?
+#       redirect_to action: :index
+#     end
+#   end
+# end
