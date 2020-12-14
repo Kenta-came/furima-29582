3b81240bbf932b281dfa33bcf31ba975817485d5
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-14 11:14:53 +0900

    コードの微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+
+
   private
+
 
   def configure_permitted_parameters
     added_attrs = [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday]
