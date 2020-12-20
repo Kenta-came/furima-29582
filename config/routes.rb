2ee640d151779d22d7612351bed274839bbd7454
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-21 00:07:56 +0900

    リソースの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :users
+  
+  resources :users 
+  resources :items, only:[:index, :new, :create]
+  
 end
