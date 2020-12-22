ed35f808a4df0c0126dbec6b64023e3638bc9c15
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2020-12-23 01:46:38 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -6,7 +7,11 @@ class ApplicationController < ActionController::Base
 
 
   private
-
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'nimu' && password == '6666'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
