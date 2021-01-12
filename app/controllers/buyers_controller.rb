5a45fdbfd981c257210d521df66b8ada8906a482
Kenta-came <65546166+Kenta-came@users.noreply.github.com>
2021-01-12 14:51:23 +0900

    クレジット機能の追加

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -9,6 +9,7 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @transaction = Transaction.new(transaction_params)  
      if @transaction.valid?
+        pay_item
         @transaction.save
         redirect_to root_path
      else
@@ -19,6 +20,16 @@ class BuyersController < ApplicationController
   private
    # 全てのストロングパラメーターを1つに統合
   def transaction_params
-   params.require(:transaction).permit(:post_num, :ship_pref_id, :city, :house_num, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:taken], price: item.price)
+   params.require(:transaction).permit(:post_num, :ship_pref_id, :city, :house_num, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: @item.price)
   end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: transaction_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,15 +1,17 @@
 class Transaction
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :buyer_id, :post_num, :ship_pref_id, :city, :house_num, :building, :tel
+  attr_accessor :item_id, :user_id, :buyer_id, :post_num, :ship_pref_id, :city, :house_num, :building, :tel, :token, :price
 
   with_options presence: { message: 'を入力してください！'} do
     validates :post_num, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'ハイフンを入力してください！'}
     validates :ship_pref_id, numericality: {other_than: 1, message: 'の選択を行なってください！'} 
     validates :city
     validates :house_num
-    validates :building
     validates :tel, format: { with: /\A\d{11}\z/, message: 'ハイフンを入力してください！'}
   end
+
+  validates :price, presence: true
+  validates :token, presence: true
 
   def save
     buyer = Buyer.create(user_id: user_id, item_id: item_id)
